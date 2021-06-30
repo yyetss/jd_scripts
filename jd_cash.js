e7c3fd2d913d7f92aaf8939bab4b8574f38620a1
yyetss <757542306@qq.com>
2021-06-30 15:10:42 +0800

    Add files via upload

--- jd_cash.js
+++ jd_cash.js
@@ -47,6 +47,8 @@ let cashinviteCode11 = '9pSEu103v1aPKMlR';
 let cashinviteCode12 = 'eU9Ya7nnNfR3p2mAnnRG3w';
 let cashinviteCode13 = 'eU9YJLvaFaVRpyyusAtO';
 let cashinviteCode14 = 'ZEE2b--7bvgg9G7SzA';
+let cashinviteCode15 = 'eU9YCKzPIqp9pwaEljpP';
+let cashinviteCode16 = 'eU9YPbrPN4J0mDyVqRZX';
 if (process.env.cashinviteCode) {
   cashinviteCode = process.env.cashinviteCode;
 }
@@ -89,7 +91,13 @@ if (process.env.cashinviteCode13) {
 if (process.env.cashinviteCode14) {
   cashinviteCode14 = process.env.cashinviteCode14;
 }
-newShareCodes = [{"inviteCode":`${cashinviteCode}`},{"inviteCode":`${cashinviteCode2}`},{"inviteCode":`${cashinviteCode3}`},{"inviteCode":`${cashinviteCode4}`},{"inviteCode":`${cashinviteCode5}`},{"inviteCode":`${cashinviteCode6}`},{"inviteCode":`${cashinviteCode7}`},{"inviteCode":`${cashinviteCode8}`},{"inviteCode":`${cashinviteCode9}`},{"inviteCode":`${cashinviteCode10}`},{"inviteCode":`${cashinviteCode11}`},{"inviteCode":`${cashinviteCode12}`},{"inviteCode":`${cashinviteCode13}`},{"inviteCode":`${cashinviteCode14}`}] //这里修改你的邀请码
+if (process.env.cashinviteCode15) {
+  cashinviteCode15 = process.env.cashinviteCode15;
+}
+if (process.env.cashinviteCode16) {
+  cashinviteCode16 = process.env.cashinviteCode16;
+}
+newShareCodes = [{"inviteCode":`${cashinviteCode}`},{"inviteCode":`${cashinviteCode2}`},{"inviteCode":`${cashinviteCode3}`},{"inviteCode":`${cashinviteCode4}`},{"inviteCode":`${cashinviteCode5}`},{"inviteCode":`${cashinviteCode6}`},{"inviteCode":`${cashinviteCode7}`},{"inviteCode":`${cashinviteCode8}`},{"inviteCode":`${cashinviteCode9}`},{"inviteCode":`${cashinviteCode10}`},{"inviteCode":`${cashinviteCode11}`},{"inviteCode":`${cashinviteCode12}`},{"inviteCode":`${cashinviteCode13}`},{"inviteCode":`${cashinviteCode14}`},{"inviteCode":`${cashinviteCode15}`},{"inviteCode":`${cashinviteCode16}`}] //这里修改你的邀请码
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
     cookiesArr.push(jdCookieNode[item])
