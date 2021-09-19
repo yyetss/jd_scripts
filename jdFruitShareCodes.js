40500f40cfee7b9703aa091cf90340d1c1c99623
yyetss <757542306@qq.com>
2021-09-19 10:13:30 +0800

    Add files via upload

---
+++ jdFruitShareCodes.js
@@ -0,0 +1,37 @@
+/*
+东东农场互助码
+此文件为Node.js专用。其他用户请忽略
+支持京东N个账号
+ */
+//云服务器腾讯云函数等NOde.js用户在此处填写京东东农场的好友码。
+// 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
+// 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
+let FruitShareCodes = [
+ '64304080a2714e1cac59af03b0009581@e9333dbf9c294ad6af2792dacc236fe7@f6f58dc91bad4e24b9dd6f9a1ba19950@4f53be3edea541268b1b948456d6ff4e@674922141a014f13bdd882e8b5c15916@51639c4ddebd431b842fb10089ade73f@ceffcfd7afee4553999b660982e45275@010abbafb641440ca25a5bd8f23ec477@282db86a2e9b494d918563ed78125377@b6fc2988ac6740f092a2ee3da40ebd74@8ab904ca73c74eb4a94206b87f5c3a99@4f205d80700c4591a1484b739503e4b7@b357c1247d5d43b5b196fce423e3097f@e941a4c3eb7748e78a37c95f5ed2b24a@0673940819484a88bc375b2311e2f740@304b39f17d6c4dac87933882d4dec6bc@3e6f0b7a2d054331a0b5b956f36645a9',
+ '64304080a2714e1cac59af03b0009581@e9333dbf9c294ad6af2792dacc236fe7@f6f58dc91bad4e24b9dd6f9a1ba19950@4f53be3edea541268b1b948456d6ff4e@674922141a014f13bdd882e8b5c15916@51639c4ddebd431b842fb10089ade73f@ceffcfd7afee4553999b660982e45275@010abbafb641440ca25a5bd8f23ec477@282db86a2e9b494d918563ed78125377@b6fc2988ac6740f092a2ee3da40ebd74@8ab904ca73c74eb4a94206b87f5c3a99@4f205d80700c4591a1484b739503e4b7@b357c1247d5d43b5b196fce423e3097f@e941a4c3eb7748e78a37c95f5ed2b24a@0673940819484a88bc375b2311e2f740@304b39f17d6c4dac87933882d4dec6bc@3e6f0b7a2d054331a0b5b956f36645a9',
+]
+
+// 从日志获取互助码
+// const logShareCodes = require('./utils/jdShareCodes');
+// if (logShareCodes.FRUITSHARECODES.length > 0 && !process.env.FRUITSHARECODES) {
+//   process.env.FRUITSHARECODES = logShareCodes.FRUITSHARECODES.join('&');
+// }
+
+// 判断github action里面是否有东东农场互助码
+if (process.env.FRUITSHARECODES) {
+  if (process.env.FRUITSHARECODES.indexOf('&') > -1) {
+    console.log(`您的东东农场互助码选择的是用&隔开\n`)
+    FruitShareCodes = process.env.FRUITSHARECODES.split('&');
+  } else if (process.env.FRUITSHARECODES.indexOf('\n') > -1) {
+    console.log(`您的东东农场互助码选择的是用换行隔开\n`)
+    FruitShareCodes = process.env.FRUITSHARECODES.split('\n');
+  } else {
+    FruitShareCodes = process.env.FRUITSHARECODES.split();
+  }
+} else {
+  console.log(`由于您环境变量(FRUITSHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+}
+for (let i = 0; i < FruitShareCodes.length; i++) {
+  const index = (i + 1 === 1) ? '' : (i + 1);
+  exports['FruitShareCode' + index] = FruitShareCodes[i];
+}
---
+++ jdPetShareCodes.js
@@ -0,0 +1,37 @@
+/*
+东东萌宠互助码
+此文件为Node.js专用。其他用户请忽略
+支持京东N个账号
+ */
+//云服务器腾讯云函数等NOde.js用户在此处填写东东萌宠的好友码。
+// 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
+// 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
+let PetShareCodes = [
+  'MTE1NDAxNzcwMDAwMDAwNDE3MDkwNzE=@MTE1NDUyMjEwMDAwMDAwNDE3NDU2MjU=@MTEzMzI0OTE0NTAwMDAwMDA0MTc0NTcxMw==@MTE1NDQ5OTIwMDAwMDAwNDQyNTAwMjM=@MTE1NDQ5MzYwMDAwMDAwNDQyMjg4OTc=@MTE1NDAxNzgwMDAwMDAwNDI1NDcwMzU=@MTE1NDQ5MzYwMDAwMDAwNDUzNjI4NjM=@MTE1NDQ5OTUwMDAwMDAwNDUzNjI4NjU=@MTE0MDQ3MzEwMDAwMDAwNDk0Mzk1NDU=@MTEzMzI0OTE0NTAwMDAwMDA0NDU1ODUxNw==@MTE1NDQ5OTUwMDAwMDAwNDQ4ODg3NjE=@MTEzMzI0OTE0NTAwMDAwMDA0NDg4ODkyNQ==@MTAxODc2NTEzMjAwMDAwMDAzMjcyNDgyMQ==@MTAxODc2NTEzMzAwMDAwMDAxNDEzNTM0Nw==@MTAxODcxOTI2NTAwMDAwMDAwMjM1OTI5OQ==@MTE1NDQ5MzYwMDAwMDAwMzk2NTY2MTE=@MTE1NDQ5OTUwMDAwMDAwMzk2NTY2MTk=',
+  'MTE1NDAxNzcwMDAwMDAwNDE3MDkwNzE=@MTE1NDUyMjEwMDAwMDAwNDE3NDU2MjU=@MTEzMzI0OTE0NTAwMDAwMDA0MTc0NTcxMw==@MTE1NDQ5OTIwMDAwMDAwNDQyNTAwMjM=@MTE1NDQ5MzYwMDAwMDAwNDQyMjg4OTc=@MTE1NDAxNzgwMDAwMDAwNDI1NDcwMzU=@MTE1NDQ5MzYwMDAwMDAwNDUzNjI4NjM=@MTE1NDQ5OTUwMDAwMDAwNDUzNjI4NjU=@MTE0MDQ3MzEwMDAwMDAwNDk0Mzk1NDU=@MTEzMzI0OTE0NTAwMDAwMDA0NDU1ODUxNw==@MTE1NDQ5OTUwMDAwMDAwNDQ4ODg3NjE=@MTEzMzI0OTE0NTAwMDAwMDA0NDg4ODkyNQ==@MTAxODc2NTEzMjAwMDAwMDAzMjcyNDgyMQ==@MTAxODc2NTEzMzAwMDAwMDAxNDEzNTM0Nw==@MTAxODcxOTI2NTAwMDAwMDAwMjM1OTI5OQ==@MTE1NDQ5MzYwMDAwMDAwMzk2NTY2MTE=@MTE1NDQ5OTUwMDAwMDAwMzk2NTY2MTk=',
+]
+
+// 从日志获取互助码
+// const logShareCodes = require('./utils/jdShareCodes');
+// if (logShareCodes.PETSHARECODES.length > 0 && !process.env.PETSHARECODES) {
+//   process.env.PETSHARECODES = logShareCodes.PETSHARECODES.join('&');
+// }
+
+// 判断github action里面是否有东东萌宠互助码
+if (process.env.PETSHARECODES) {
+  if (process.env.PETSHARECODES.indexOf('&') > -1) {
+    console.log(`您的东东萌宠互助码选择的是用&隔开\n`)
+    PetShareCodes = process.env.PETSHARECODES.split('&');
+  } else if (process.env.PETSHARECODES.indexOf('\n') > -1) {
+    console.log(`您的东东萌宠互助码选择的是用换行隔开\n`)
+    PetShareCodes = process.env.PETSHARECODES.split('\n');
+  } else {
+    PetShareCodes = process.env.PETSHARECODES.split();
+  }
+} else {
+  console.log(`由于您环境变量(PETSHARECODES)里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
+}
+for (let i = 0; i < PetShareCodes.length; i++) {
+  const index = (i + 1 === 1) ? '' : (i + 1);
+  exports['PetShareCode' + index] = PetShareCodes[i];
+}
